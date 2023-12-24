a7984bfbdcd1e2d86c6568c99eb50989f5586c2a
loki-lab <nguyenvanminhcuong2002@gmail.com>
2023-12-24 10:41:36 +0700

    latest version

--- lightning_model.py
+++ lightning_model.py
@@ -1,5 +1,5 @@
 from torch import nn
-from model import VGG16
+from model import VGG16, LeNet5
 from pytorch_lightning import LightningModule
 from torchmetrics.classification import Accuracy
 from torch.optim import Adam
@@ -9,6 +9,46 @@ class VGG16Lightning(LightningModule):
     def __init__(self, num_classes, train_ds, val_ds):
         super().__init__()
         self.model = VGG16(num_classes=num_classes)
+        self.accuracy = Accuracy(task="multiclass", num_classes=num_classes)
+        self.train_ds = train_ds 
+        self.val_ds = val_ds
+    
+    def forward(self, x):
+        out = self.model(x)
+        return out
+    
+    def training_step(self, input, input_idx):
+        image, label = input
+        output = self.forward(image)
+        loss = nn.CrossEntropyLoss()(output, label)
+        accuracy = self.accuracy(output, label)
+        self.log("train_loss_step", loss, on_step=False, on_epoch=True)
+        self.log("train_acc_step", accuracy, on_step=False, on_epoch=True)
+        return loss
+
+    def validation_step(self, input, input_idx):
+        image, label = input
+        output = self.forward(image)
+        loss = nn.CrossEntropyLoss()(output, label)
+        accuracy = self.accuracy(output, label)
+        self.log("val_loss_step", loss, on_step=False, on_epoch=True)
+        self.log("val_acc_step", accuracy, on_step=False, on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        return Adam(self.model.parameters(), lr=0.0001)
+    
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=32, shuffle=True, num_workers=2)
+    
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=32, shuffle=False, num_workers=2)
+   
+    
+class LeNet5Lightning(LightningModule):
+    def __init__(self, num_classes, train_ds, val_ds):
+        super().__init__()
+        self.model = LeNet5(num_classes=num_classes)
         self.accuracy = Accuracy(task="multiclass", num_classes=num_classes)
         self.train_ds = train_ds 
         self.val_ds = val_ds
